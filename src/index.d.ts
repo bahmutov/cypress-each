8a9d578efa326484514fb2503458bcf5856f4ce9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-18 14:38:44 -0500

    feat: add predicate filter (#21)
    ## Custom filter predicate
    
    You can filter the items by passing a predicate function
    
    ```js
    it.each(items, (x, k) => ...)
    // creates a test for every item the predicate returns a truthy value
    ```

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,7 @@
 // https://github.com/bahmutov/cypress-each
 
 type TestTitleFn<T> = (item: T, index: number, items: T[]) => string
+type ItemPredicateFunction<T> = (item: T, index: number, items: T[]) => boolean
 
 declare namespace Mocha {
   type TestCallback<T> = (this: Context, arg0: T, arg1: any, arg2: any) => void
@@ -12,14 +13,14 @@ declare namespace Mocha {
      * Iterates over each given item (optionally chunked), and creates
      * a separate test for each one.
      * @param values Input items to create the tests form OR number of times to repeat a test
-     * @param totalChunks (Optional) number of chunks to split the items into
+     * @param totalChunks (Optional) number of chunks to split the items into, or Nth filter, or a predicate function
      * @param chunkIndex (Optional) index of the chunk to get items from
      * @example it.each([1, 2, 3])('test %K', (x) => ...)
      * @see https://github.com/bahmutov/cypress-each
      */
     each<T = unknown>(
       values: T[] | number,
-      totalChunks?: number,
+      totalChunks?: number | ItemPredicateFunction<T>,
       chunkIndex?: number,
     ): (titlePattern: string | TestTitleFn<T>, fn: TestCallback<T>) => void
   }
