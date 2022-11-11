e77ce52a11ebc39b7b26b6ca3627be1b9215e5ac
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-11 15:28:38 -0500

    add failing test for the test object

---
+++ cypress/e2e/object-input.cy.ts
@@ -0,0 +1,28 @@
+import '../../src'
+
+function add(a: number, b: number) {
+  return a + b
+}
+
+const testCases = {
+  // key: the test label
+  // value: list of inputs for each test case
+  'positive numbers': [1, 6, 7], // [a, b, expected result]
+  'negative numbers': [1, -6, -5],
+}
+
+describe('converted to an array of arrays', () => {
+  // we can convert the above object ourselves
+  const pairs = Cypress._.toPairs(testCases)
+  it.each(pairs)('testing %0', (title, numbers) => {
+    const [a, b, expectedResult] = numbers
+    expect(add(a, b)).to.equal(expectedResult)
+  })
+})
+
+// https://github.com/bahmutov/cypress-each/issues/53
+describe.skip('automatic conversion', () => {
+  it.each(testCases)((a, b, expectedResult) => {
+    expect(add(a, b)).to.equal(expectedResult)
+  })
+})
