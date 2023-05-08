cbdfe9c476190450b31dca9bbf3418e797a4a005
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-05-08 12:08:43 -0400

    fix: ignore title position

--- cypress/e2e/title-position.cy.js
+++ cypress/e2e/title-position.cy.js
@@ -6,9 +6,11 @@ import '../../src'
 describe('Positional title arguments', () => {
   const list = [['foo', 'main']]
 
-  it.each(list)('title is %1 and %0', function (a, b) {
-    expect(a, 'first').to.equal(a)
-    expect(b, 'second').to.equal(b)
+  // https://github.com/bahmutov/cypress-each/issues/76
+  // @ts-ignore
+  it.each(list)('title is %1 and %0', (a, b) => {
+    expect(a, 'first').to.equal('foo')
+    expect(b, 'second').to.equal('main')
     // @ts-ignore
     expect(this.test.title, 'computed test title').to.equal(
       'title is main and foo',
