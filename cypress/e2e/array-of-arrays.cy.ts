012361b354f1c223f336f9d02ad4530c4d564714
Ihor Dluhosh <idlugosh@pgs-soft.com>
2023-05-08 15:08:08 +0200

    add array of arrays type

--- cypress/e2e/array-of-arrays.cy.ts
+++ cypress/e2e/array-of-arrays.cy.ts
@@ -1,14 +1,15 @@
 import '../../src'
 
-// https://github.com/bahmutov/cypress-each/issues/2
 describe('Array of arrays', () => {
   it.each([
-    [1, 'foo'],
-    [2, 'bar'],
-  ])('test with %d %s', (a, b) => {
+    [1, 'foo', true],
+    [2, 'bar', false],
+  ])('test with %d %s', (a: number, b: string, c: boolean) => {
     // a should be a number
     // b should be a string
+    // b should be a boolean
     expect(a, 'first argument').to.be.a('number')
     expect(b, 'second argument').to.be.a('string')
+    expect(c, 'third argument').to.be.a('boolean')
   })
 })
